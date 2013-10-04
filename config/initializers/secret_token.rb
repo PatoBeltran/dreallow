b8fc44ffeb0e2013c9aae2265e2cbf98bbd2ec0d
PatoBeltran <pbelsal@gmail.com>
2013-10-04 17:39:28 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dreallow::Application.config.secret_key_base = 'cbb74d1ed04298d665895dc5cabc074168571a50eaf9e79c62f4814de4fed8f6c9f3593c66f3cd4c33841a7ae4917d68bbfde413fd29cb7f2171aa6237e5aeb8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dreallow::Application.config.session_store :cookie_store, key: '_dreallow_session'
