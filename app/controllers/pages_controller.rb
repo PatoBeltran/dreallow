4b2e9ae54216e542151b65ba6cb27b7c7d411ddb
PatoBeltran <pbelsal@gmail.com>
2013-10-05 10:19:08 -0500

    nadamas para no perder este progreso

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def landing
-    @trending = Dream.all.order('created_at DESC').limit(8)
+    unless current_user.present?
+      @trending = Dream.all.order('created_at DESC').limit(8)
+    else
+      redirect_to dreams_path
+    end
   end
 end
